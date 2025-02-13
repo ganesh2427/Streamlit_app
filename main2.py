e6f375f0103ea9a87934bdf899f9aa30abfbeb17
ganesh2427 <166892996+ganesh2427@users.noreply.github.com>
2025-02-13 10:13:43 +0530

    new

---
+++ main2.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+def home_page():
+    st.title("Hi")
+    st.write("Welcome ")
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
+
+# # Set background color using markdown and CSS
+# st.markdown(
+#     """
+#     <style>
+#         .main {
+#             background-color: #f0f2f6;
+#         }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# Sidebar navigation with Home button
+st.sidebar.title("Content")
+if st.sidebar.button("Home Page"):
+    st.session_state.page = "home"
+    st.rerun()
+
+app_mode = st.sidebar.selectbox("Choose a project", ["Select a project", "Plant Leaf Diesease Detection", "Face Recognition", "OCR", "Suduko"], index=0)
+
+if app_mode != "Select a project":
+    st.session_state.page = app_mode
+
+# Display content based on selection
+if st.session_state.page == "home":
+    home_page()
+elif st.session_state.page == "Plant Leaf Diesease Detection":
+    st.markdown("""<h1 style='font-size:36px;'>Plant Leaf Diesease Detection</h1>""", unsafe_allow_html=True)
+    st.write("Details about Project.")
+elif st.session_state.page == "Face Recognition":
+    st.markdown("""<h1 style='font-size:36px;'>Face Recognition</h1>""", unsafe_allow_html=True)
+    st.write("Details about Project.")
+elif st.session_state.page == "OCR":
+    st.markdown("""<h1 style='font-size:36px;'>OCR</h1>""", unsafe_allow_html=True)
+    st.write("Details about Project.")
+elif st.session_state.page == "Suduko":
+    st.markdown("""<h1 style='font-size:36px;'>Suduko</h1>""", unsafe_allow_html=True)
+    st.write("Details about Project.")
